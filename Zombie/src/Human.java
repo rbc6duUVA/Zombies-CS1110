f45b1009ccaaaef0f8eb8ecf702648334863bfc8
rbc6duUVA <rbc6du@virginia.edu>
2014-11-13 16:48:07 -0500

    Update Human.java

--- Zombie/src/Human.java
+++ Zombie/src/Human.java
@@ -1,14 +1,10 @@
 import java.awt.Rectangle;
+import java.util.Vector;
 
 /**
  * Human.java
  * 
- * The player's character. This class should have fields that can represent the
- * players' current location, the location to which it is trying to go, a
- * relative speed, and a number of bombs, and a Rectangle representing the
- * player's hitbox. Other fields for the size of the player and the hitbox may
- * be useful, along with frame information if you do animation. You should
- * create methods for the following: 1. checking to see if the player's hitbox
+ * 1. checking to see if the player's hitbox
  * Rectangle is colliding with any other hitbox Rectangle (just obstacle
  * Rectangles - Zombies can handle Human collision) 2. movement (normal movement
  * and what to do if there is a collision) 3. constructors 4. getters/setters 5.
@@ -27,8 +23,12 @@ public class Human {
 	private double tryY;
 	private double dx;
 	private double dy;
+	private double speed;
 	private int numOfBombs;
 	Rectangle hitbox;
+	private int width;
+	private int height;
+	private int[][] spriteImage;
 	
 	public double getX() {
 		return x;
@@ -36,19 +36,53 @@ public class Human {
 	public double getY() {
 		return y;
 	}
+	public double getDX() {
+		return dx;
+	}
+	public double getDY() {
+		return dy;
+	}
+	public void setTryX(double tX, double tY) {
+		tryX = tX;
+		tryY = tY;
+	}
+	public Rectangle getHitbox() {
+		return hitbox;
+	}
 	
 	public Human() {
 		x=300;
 		y=300;
-		tryX=x;
-		tryY=y;
+		tryX=0;
+		tryY=0;
 		dx=0;
 		dy=0;
+		speed=20;
 		numOfBombs = 3;
-		hitbox = new Rectangle( (int) x , (int) y , 38 , 74 );
-
-				
+		width = 38;
+		height = 74;
+		hitbox = new Rectangle( (int) x , (int) y , width , height );	
 	}
-	
-
+	/*
+	2. movement (normal movement
+ 	 * and what to do if there is a collision) 3. constructors 4. getters/setters 5.
+ 	 * bomb counting 6. anything else you may need.
+	 */
+	public boolean getCollision(Rectangle target) {
+		return target.intersects(this.hitbox);
+	}
+	public void homeMouse(){
+		//calculate the unit vector of of Human to Desired Position;
+		double deltaX = tryX - x;
+		double deltaY = tryY - y;
+		double xWard = deltaX / Math.sqrt((deltaX*deltaX) + (deltaY*deltaY));
+		double yWard = deltaY / Math.sqrt((deltaX*deltaX) + (deltaY*deltaY));
+		
+		//Scale unit vector to speed of Human
+		dx = speed*xWard;
+		dy = speed*yWard;
+	}
+		
+		
+		
 }
