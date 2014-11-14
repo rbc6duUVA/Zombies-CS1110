cdccd4da58c75858171461b65692e7e1bc02a7af
rbc6duUVA <rbc6du@virginia.edu>
2014-11-13 23:42:12 -0500

    Update Human.java

--- Zombie/src/Human.java
+++ Zombie/src/Human.java
@@ -30,6 +30,8 @@ public class Human {
 	private int height;
 	private int[][] spriteImage;
 	
+	Rectangle killUP, killDOWN, killLEFT, killRIGHT;
+	
 	public double getX() {
 		return x;
 	}
@@ -42,12 +44,18 @@ public class Human {
 	public double getDY() {
 		return dy;
 	}
-	public void setTryX(double tX, double tY) {
+	public void setTry(double tX, double tY) {
 		tryX = tX;
 		tryY = tY;
 	}
 	public Rectangle getHitbox() {
 		return hitbox;
+	}
+	public int getNumOfBombs() {
+		return numOfBombs;
+	}
+	public void addNumOfBombs(int increment) {
+		numOfBombs += increment;
 	}
 	
 	public Human() {
@@ -55,20 +63,31 @@ public class Human {
 		y=300;
 		tryX=0;
 		tryY=0;
-		dx=0;
-		dy=0;
-		speed=20;
+		dx=1;
+		dy=1;
+		speed=40;
 		numOfBombs = 3;
-		width = 38;
-		height = 74;
-		hitbox = new Rectangle( (int) x , (int) y , width , height );	
+		width = 37;
+		height = 73;
+		hitbox = new Rectangle( (int) x , (int) y , width , height );
+		
+		killUP = 	new Rectangle( (int) x 				, (int) y-1 				, width , 1);
+		killDOWN = 	new Rectangle( (int) x 				, (int) (y + height + 1)	, width , 1);
+		killLEFT = 	new Rectangle( (int) (x-1) 			, (int) y 					, 1 	, height);
+		killRIGHT = new Rectangle( (int) (x + width + 1), (int) y 					, 1 	, height);
+		
 	}
-	/*
-	2. movement (normal movement
- 	 * and what to do if there is a collision) 3. constructors 4. getters/setters 5.
- 	 * bomb counting 6. anything else you may need.
+
+	/**
+	 * MUST RUN BEFORE HUMAN.MOVE(time)!!!
+	 * @param target
+	 * @return
 	 */
 	public boolean getCollision(Rectangle target) {
+		if( (target.intersects(killUP)) && (dy < 0) ) {dy = 0;}
+		if( (target.intersects(killDOWN)) && (dy > 0) ) {dy = 0;}
+		if( (target.intersects(killLEFT)) && (dx < 0) ) {dx = 0;}
+		if( (target.intersects(killRIGHT)) && (dy < 0) ) {dx = 0;}
 		return target.intersects(this.hitbox);
 	}
 	public void homeMouse(){
@@ -82,7 +101,16 @@ public class Human {
 		dx = speed*xWard;
 		dy = speed*yWard;
 	}
-		
-		
+	public void move(double ellapsedTime) {
+		x += dx*ellapsedTime;
+		y += dy*ellapsedTime;
+		hitbox.translate((int) (dx*ellapsedTime), (int) (dy*ellapsedTime));
+		killUP.translate((int) (dx*ellapsedTime), (int) (dy*ellapsedTime));
+		killDOWN.translate((int) (dx*ellapsedTime), (int) (dy*ellapsedTime));
+		killRIGHT.translate((int) (dx*ellapsedTime), (int) (dy*ellapsedTime));
+		killLEFT.translate((int) (dx*ellapsedTime), (int) (dy*ellapsedTime));
+		homeMouse();
+	}
+
 		
 }
