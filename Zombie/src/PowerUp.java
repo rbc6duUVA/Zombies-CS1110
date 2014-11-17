46c121a74e28fb42ec260caebba4a8596f7c101c
rbc6duUVA <rbc6du@virginia.edu>
2014-11-17 18:08:47 -0500

    Update PowerUp.java

--- Zombie/src/PowerUp.java
+++ Zombie/src/PowerUp.java
@@ -1,4 +1,3 @@
-import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
 
@@ -23,34 +22,25 @@ public class PowerUp {
 	public Rectangle getHitbox() {
 		return hitbox;
 	}
-	
-	public void drawPowerUp(SimpleCanvas canvas, Graphics2D g) {
-		age++;
-		switch (id) {
-			case 0: 
-				phase++;
-				if(phase>=4) {phase=0;}
-				if(id==0) {
-					canvas.drawPowerUp_Bomb(g, x, y, phase);
-				} break;
-			
-			case 1: 
-				phase++;
-				if(phase>=4) {phase=0;}
-				if(id==0) {
-					canvas.drawPowerUp_Bomb(g, x, y, phase);
-				} break;
-				
-			default:
-				phase++;
-				break;
-		}
-		
+	public int getX() {
+		return x;
 	}
-	
+	public int getY() {
+		return y;
+	}
+	public int getID() {
+		return id;
+	}
+	public int getPhase() {
+		return phase;
+	}
+	public int getAge() {
+		return age;
+	}
 	public boolean isAlive() {
-		if(age<=5000) return true;
-		if(age>5000) return false;
+		age++;
+		if(age<=15000) return true;
+		if(age>15000) return false;
 		return true;
 	}
 }
