9fb3d75e3350042ec7ac9b2f990fb75f8bc48b84
rbc6duUVA <rbc6du@virginia.edu>
2014-11-20 22:57:27 -0500

    Update ZombieSurvival.java

--- Zombie/src/ZombieSurvival.java
+++ Zombie/src/ZombieSurvival.java
@@ -36,12 +36,16 @@ public class ZombieSurvival {
 	private int phaseH;
 	private int phaseZ;
 	private int phasePUB;
+	private int phasePUS;
+	private int phasePUI;
+	private boolean isInvincible;
 	boolean gameover;
+	private int playerDrawMode;
 	
-	private PowerUp extraBomb;
+	private PowerUp thePowerUp;
 	
 	private boolean bombExploded;
-	private boolean powerUpBombActive;
+	private boolean powerUpActive;
 	private boolean wasActivated;
 	
 
@@ -50,9 +54,12 @@ public class ZombieSurvival {
 		output = new InfoFrame(this);
 		player = new Human();
 		zombies = new ArrayList<Zombie>();
+			zombies.add(new Zombie(350,-500));
 		obstacles = new ArrayList<Rectangle>();
-		extraBomb = new PowerUp(-50,-50,0);
+		thePowerUp = new PowerUp(-50,-50,0);
 		score = 0;
+		playerDrawMode = 0;
+		isInvincible = false;
 		
 		ticks = 0;
 		boomTick = 0;
@@ -60,11 +67,13 @@ public class ZombieSurvival {
 		phaseH = 0;
 		phaseZ = 0;
 		phasePUB = 0;
+		phasePUS = 0;
+		phasePUI = 0;
 		
 		gameover = false;
 		
 		bombExploded = false;
-		powerUpBombActive = false;
+		powerUpActive = false;
 		wasActivated = false;
 		
 		loadObstacles("course.csv");
@@ -96,21 +105,28 @@ public class ZombieSurvival {
 			}
 		}
 		if (button == 3) {
-			output.println("You clicked the right mouse button!");
+			if(player.getNumOfBombs() > 0) {
+				player.addNumOfBombs(-1);
+				output.println("BOOM!\tYou have "+player.getNumOfBombs()+" bombs remaining");
+				detonateBomb();
+				bombExploded = true;
+			} else {
+			output.println("Out of Bombs!");
+			}
 		}
 	}
 
 	public void detonateBomb() {
 		int numZombKilled=0;
-		Rectangle bomb = new Rectangle((int) (player.getX()-75) , (int) (player.getY()-50) , 200 , 200 );
+		Rectangle bomb = new Rectangle((int) (player.getX()-100) , (int) (player.getY()-100) , 300 , 300 );
 		for(int i=0; i<zombies.size(); i++) {
-			if(bomb.intersects(zombies.get(i).getHitbox())) {
+			if(bomb.intersects(zombies.get(i).getCollisionBox())) {
 				zombies.remove(i);
 				numZombKilled++;
 			}
 		}
-		score = (int) (score + numZombKilled*Math.sqrt(numZombKilled));
-		output.println("[Killed "+ numZombKilled +" zombies]");
+		score = (int) (score + numZombKilled * ( (int) (Math.sqrt(numZombKilled) + 0.5)) );
+		output.println("[Killed "+ numZombKilled +" zombies]\t("+numZombKilled * ((int) (Math.sqrt(numZombKilled) +0.5))+"pts)");
 	}
 
 	public boolean draw(Graphics2D g, float elapsedTime) {
@@ -127,7 +143,7 @@ public class ZombieSurvival {
 		
 		//Drawing and moving the player
 		player.move(elapsedTime);	//Must occur after "getCollision()" is called -- how the Human class works.
-		canvas.drawHuman(g, player, player.getDirection(), phaseH);
+		canvas.drawHuman(g, player, playerDrawMode+player.getDirection(), phaseH);
 
 		////////////////////////////////////////////////////////////////////
 		//Time sensitive events
@@ -137,7 +153,10 @@ public class ZombieSurvival {
 			if(phaseH==8) { phaseH=0; }
 			phasePUB++;
 			if(phasePUB==4) { phasePUB=0; }
-			
+			phasePUS++;
+			if(phasePUS==6) { phasePUS=0; }
+			phasePUI++;
+			if(phasePUI==5) { phasePUI=0; }
 		}
 		if(ticks%200 == 0) {
 			phaseZ++;
@@ -148,19 +167,23 @@ public class ZombieSurvival {
 			int zy = (int) (Math.random()*(BOARDHEIGHT-81));
 			boolean conflict = false;
 			
-			Confliction:
+			Confliction1:
 			for(int i=0; i<obstacles.size(); i++) {
 				if(new Rectangle(zx,zy,50,90).intersects(obstacles.get(i))) {
 					conflict = true;
-					break Confliction;
-				}
-				if(new Rectangle(zx,zy,50,90).intersects(zombies.get(i).getCollisionBox())) {
-					conflict = true;
-					break Confliction;
+					break Confliction1;
 				}
 			}
 			
-			if(new Rectangle(zx,zy,90,130).intersects(player.getHitbox())) { conflict = true; }
+			Confliction2:
+			for(int i=0; i<zombies.size(); i++){
+				if(new Rectangle(zx,zy,50,90).intersects(zombies.get(i).getCollisionBox())) {
+					conflict = true;
+					break Confliction2;
+				}
+			}
+			
+			if(new Rectangle(zx,zy,90,130).intersects(player.getSpawnbox())) { conflict = true; }
 			
 			if(!conflict) { zombies.add(new Zombie(zx,zy)); }
 		}		
@@ -169,23 +192,26 @@ public class ZombieSurvival {
 			output.println("New Bomb Acquired! ("+player.getNumOfBombs()+")");
 		}
 		if(ticks%35000 == 0) {
-			powerUpBombActive = true;
+			powerUpActive = true;
 			wasActivated = true;
 		}
 		if(ticks == 100001) 		{ ticks = 1; }
 		////////////////////////////////////////////////////////////////////
 		
-		
 		//Zombie actions go here
-		for(int i=0; i<zombies.size(); i++) {
-			canvas.drawZombie(g, zombies.get(i), zombies.get(i).getDirection(), phaseZ);
-			zombies.get(i).setTry(player.getX(),player.getY());
-			if(zombies.get(i).playerCollision(player.getHitbox())) { gameover = true; }
-			for(int j=0; j<zombies.size(); j++) {
-				if(i != j) { zombies.get(i).getObstacleCollision(zombies.get(j).getCollisionBox()); }
+		
+			for(int i=0; i<zombies.size(); i++) {
+				canvas.drawZombie(g, zombies.get(i), zombies.get(i).getDirection(), phaseZ);
+				zombies.get(i).setTry(player.getX(),player.getY());
+				
+				if(!isInvincible) {
+					if(zombies.get(i).playerCollision(player.getHitbox())) { gameover = true; }
 				}
-			zombies.get(i).move(elapsedTime);
-		}
+				for(int j=0; j<zombies.size(); j++) {
+					if(i != j) { zombies.get(i).getObstacleCollision(zombies.get(j).getCollisionBox()); }
+					}
+				zombies.get(i).move(elapsedTime);
+			}
 		
 		////////////////////////////////////////////////////////////////////
 		// Boolean sprite animations (explosions and powerups)
@@ -201,20 +227,48 @@ public class ZombieSurvival {
 		////////////////////////////////////////////////////////////////////
 		int puX = (int) (Math.random()*BOARDWIDTH);
 		int puY = (int) (Math.random()*BOARDHEIGHT);
-		int powerUpChooser = (int) Math.random()*1;
+		int powerUpChooser = (int) (Math.random()*3);
 		//
-		if(powerUpBombActive) {
+		if(powerUpActive) {
 			if(wasActivated == true) {	
-				extraBomb = new PowerUp(puX,puY,powerUpChooser);
+				thePowerUp = new PowerUp(puX,puY,powerUpChooser);
 				wasActivated = false;
 			}
-			canvas.drawPowerUp(g, extraBomb.getX(), extraBomb.getY(), extraBomb.getID(), phasePUB);
-			if(player.getHitbox().intersects(extraBomb.getHitbox())) {
-				player.addNumOfBombs(1);
-				output.println("Power UP!  Extra Bomb! ("+ player.getNumOfBombs() +")");
-				extraBomb = new PowerUp(-50,-50,0);
+			if(thePowerUp.getID() == 0) {
+				canvas.drawPowerUp(g, thePowerUp.getX(), thePowerUp.getY(), thePowerUp.getID(), phasePUB);
+				if(player.getHitbox().intersects(thePowerUp.getHitbox())) {
+					player.addNumOfBombs(1);
+					output.println("Power UP!  Extra Bomb! ("+ player.getNumOfBombs() +")");
+					thePowerUp = new PowerUp(-50,-50,0);
+				}
 			}
-			powerUpBombActive = extraBomb.isAlive();
+			if(thePowerUp.getID() == 1) {
+				canvas.drawPowerUp(g, thePowerUp.getX(), thePowerUp.getY(), thePowerUp.getID(), phasePUS);
+				if(player.getHitbox().intersects(thePowerUp.getHitbox())) {
+					player.setSpeed(player.getSpeed() + 7);
+					output.println("Power UP!  Speed Boost!");
+					thePowerUp = new PowerUp(-50,-50,0);
+				}
+			}
+			if(thePowerUp.getID() == 2) {
+				canvas.drawPowerUp(g, thePowerUp.getX(), thePowerUp.getY(), thePowerUp.getID(), phasePUI);
+				if(player.getHitbox().intersects(thePowerUp.getHitbox())) {
+					playerDrawMode = 5;
+					isInvincible = true;
+					output.println("Power UP!  Invincibility!");
+					thePowerUp = new PowerUp(-50,-50,0);
+					thePowerUp.setLifespan(34999-17530);
+				}
+			}
+			
+			//Take care of stuff when the powerup's effect wears up
+			powerUpActive = thePowerUp.isAlive();
+			if(!thePowerUp.isAlive()) {
+				if(thePowerUp.getID() == 2) {
+					playerDrawMode = 0;
+					isInvincible = false;
+				}
+			}
 		}
 
 		////////////////////////////////////////////////////////////////////
